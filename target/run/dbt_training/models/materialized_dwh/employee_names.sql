63f0906f9b1e0b030efdcd60d24c7e5b26324015
y.shimabukuro <y.shimabukuro@gvc.co.jp>
2025-03-01 22:41:47 +0900

    initial

---
+++ target/run/dbt_training/models/materialized_dwh/employee_names.sql
@@ -0,0 +1,22 @@
+
+      
+  
+    
+
+  create  table "dbt_training"."public_test"."employee_names"
+  
+  
+    as
+  
+  (
+    
+
+select
+  "employee_id",
+  concat("first_name", ' ', last_name) as full_name
+from
+  "dbt_training"."raw"."employees"
+
+  );
+  
+  
